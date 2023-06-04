37160e59d941b9eb1f8481e673bba643f276766e
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-06-04 18:28:27 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="parent">
+      <nav className="glass">
+        <a href="./">Home</a>
+        <a href="./">About</a>
+        <a href="./">Contact</a>
+      </nav>
+      <section className="glass">
+        <button>CONNECT</button>
+      </section>
+      <footer className="glass">
+        @ssailsbury
+      </footer>
+    </div>
+  );
+}
+
+export default App;
